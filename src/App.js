f270e57b2a7bef1b768c57c00fbdb34b0a87bff2
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2023-01-12 15:34:07 +0530

    Refactored class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import Calculator from './components/Calculator';
 
-export default class App extends PureComponent {
-  render() {
-    return (
-      <Calculator />
-    );
-  }
+export default function App() {
+  return (
+    <Calculator />
+  );
 }
