b374aefd74d2409cd16340607708317f49ab0625
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2023-01-25 16:13:47 +0530

    React router and styled the website

---
+++ src/components/Quote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Quote() {
+  return (
+    <div className="quote">
+      <h3>
+        Mathematics is not about numbers, equations, computations, or algorithms:
+        it is about understanding. - William Paul Thurston
+      </h3>
+    </div>
+  );
+}
